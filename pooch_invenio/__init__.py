43175256506349773cf6b53016af4e806327938c
tibotix <tizian@seehaus.net>
2025-11-25 03:15:15 +0100

    feature: Add first implementation of a generic `InvenioRDMRepository` data repository.

--- pooch_invenio/__init__.py
+++ pooch_invenio/__init__.py
@@ -1,1 +1,3 @@
+# The version file is generated automatically by setuptools_scm
 from ._version import version as __version__
+from .repository import InvenioRDMRepository
---
+++ pooch_invenio/repository.py
@@ -0,0 +1,157 @@
+from typing import Optional
+from functools import cached_property
+
+from pooch_doi import DataRepository
+from pooch_doi.repository import DEFAULT_TIMEOUT
+
+
+class InvenioRDMRepository(DataRepository):  # pylint: disable=missing-class-docstring
+    # A URL for an issue tracker for this implementation
+    issue_tracker: Optional[str] = "https://github.com/ssciwr/pooch-invenio/issues"
+
+    # Whether the repository allows self-hosting
+    allows_self_hosting: bool = True
+
+    # Whether this repository is fully supported (meaning that all public data
+    # from this repository is accessible via pooch).
+    full_support: bool = True
+
+    # Whether this implementation performs requests to external services
+    # during initialization. We use this to minimize the execution time.
+    init_requires_requests: bool = True
+
+    @property
+    def name(self) -> str:
+        """
+        The display name of the repository.
+        """
+        return "InvenioRDM"  # pragma: no cover
+
+    @property
+    def homepage(self) -> str:
+        """
+        The homepage URL of the repository.
+        This could be the URL of the actual service or the URL of the project,
+        if it is a data repository that allows self-hosting.
+        """
+        return "https://inveniosoftware.org/products/rdm/"  # pragma: no cover
+
+    def __init__(self, doi: str, base_url: str, record_id: str):
+        self.doi = doi
+        self.base_url = base_url
+        self.record_id = record_id
+        self.archive_url = f"{base_url}/records/{record_id}"
+        self._record_details: Optional[dict] = None
+
+    @classmethod
+    def initialize(cls, doi: str, archive_url: str):
+        """
+        Initialize the data repository if the given URL points to a
+        corresponding repository.
+
+        Initializes a data repository object. This is done as part of
+        a chain of responsibility. If the class cannot handle the given
+        repository URL, it returns `None`. Otherwise a `DataRepository`
+        instance is returned.
+
+        Parameters
+        ----------
+        doi : str
+            The DOI that identifies the repository
+        archive_url : str
+            The resolved URL for the DOI
+        """
+
+        # Remove any trailing slashes
+        archive_url = archive_url.strip("/")
+
+        # Pre-flight check to match only <base_url>/records/<record_id> archive_urls.
+        parts = archive_url.split("/")
+        if len(parts) < 2 or parts[-2] != "records":
+            return None
+
+        base_url = "/".join(parts[:-2])
+        record_id = parts[-1]
+
+        response = cls._get_record_details_response(base_url, record_id)
+
+        # If we failed, this is probably not an InvenioRDM instance
+        if 400 <= response.status_code < 600:
+            return None
+
+        repository = cls(doi, base_url, record_id)
+        repository._record_details = response.json()
+        return repository
+
+    @staticmethod
+    def _get_record_details_response(base_url: str, record_id: str):
+        import requests  # pylint: disable=C0415
+
+        # We use the special mimetype to get a consistent serialization schema of records
+        # across different InvenioRDM instances.
+        return requests.get(
+            f"{base_url}/api/records/{record_id}",
+            headers={"Accept": "application/vnd.inveniordm.v1+json"},
+            timeout=DEFAULT_TIMEOUT,
+        )
+
+    @property
+    def record_details(self) -> dict:
+        if self._record_details is None:
+            self._record_details = self._get_record_details_response(
+                self.base_url, self.record_id
+            ).json()
+        return self._record_details
+
+    def _get_record_files(self) -> dict:
+        import requests  # pylint: disable=C0415
+
+        # As we already know this is an InvenioRDM Repository, we assume
+        # the "files" endpoint exists and returns a valid json response.
+        return requests.get(
+            f"{self.base_url}/api/records/{self.record_id}/files",
+            headers={"Accept": "application/json"},
+            timeout=DEFAULT_TIMEOUT,
+        ).json()
+
+    @cached_property
+    def record_files(self) -> dict:
+        entries = self.record_details["files"].get("entries")
+        if entries is None:
+            response = self._get_record_files()
+            return {entry["key"]: entry for entry in response["entries"]}
+        return entries
+
+    def download_url(self, file_name: str) -> str:
+        """
+        Use the repository API to get the download URL for a file given
+        the archive URL.
+
+        Parameters
+        ----------
+        file_name : str
+            The name of the file in the archive that will be downloaded.
+
+        Returns
+        -------
+        download_url : str
+            The HTTP URL that can be used to download the file.
+        """
+        # Check if file exists in the repository
+        if file_name not in self.record_files:
+            raise ValueError(
+                f"File '{file_name}' not found in data archive "
+                f"{self.archive_url} (doi:{self.doi})."
+            )
+        return self.record_files[file_name]["links"]["content"]
+
+    def create_registry(self) -> dict[str, str]:
+        """
+        Create a registry dictionary using the data repository's API
+
+        Returns
+        ----------
+        registry : Dict[str,str]
+            The registry dictionary.
+        """
+        return {k: v["checksum"] for k, v in self.record_files.items()}
