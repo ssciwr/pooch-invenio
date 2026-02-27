86c38d87bf390abefbcbb17ebe567087f23f9578
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2026-02-27 15:32:19 +0100

    Add Zenodo entrypoint

--- pooch_invenio/__init__.py
+++ pooch_invenio/__init__.py
@@ -1,3 +1,7 @@
 # The version file is generated automatically by setuptools_scm
 from ._version import version as __version__
-from .repository import InvenioRDMRepository, KnownInstancesInvenioRDMRepository
+from .repository import (
+    InvenioRDMRepository,
+    KnownInstancesInvenioRDMRepository,
+    ZenodoRepository,
+)
